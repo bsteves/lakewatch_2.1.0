94f5c2c852e40ed24df4f57650dc3fcd67106f91
bsteves <bsteves@gmail.com>
2013-09-25 12:58:25 -0700

    adding paperclip

--- app/models/organism.rb
+++ app/models/organism.rb
@@ -2,4 +2,8 @@ class Organism < ActiveRecord::Base
   attr_accessible :sampling_id, :sampling_type, :taxon_id, :count, :size, :image, :notes
   belongs_to :sampling
   belongs_to :taxon
+  
+
+   has_attached_file :image, :styles => {:medium => "300x300", :thumb => "100x100"}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130905200729) do
+ActiveRecord::Schema.define(:version => 20130925183627) do
 
   create_table "counties", :force => true do |t|
     t.string   "name"
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(:version => 20130905200729) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.string   "image_file_size"
   end
 
   create_table "physical_data", :force => true do |t|
