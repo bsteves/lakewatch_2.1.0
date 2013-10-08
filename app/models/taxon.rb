730a1efbb3ab83c28285cb5cb7347acfdf79be81
bsteves <bsteves@gmail.com>
2013-10-08 16:01:48 -0700

    tweaking navigation links

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -2,6 +2,13 @@ class Taxon < ActiveRecord::Base
   attr_accessible :group, :scientific_name, :common_name, :watch_list, :image, :notes
   has_many :organisms, :dependent => :destroy
 
+   has_attached_file :image, 
+#          :styles => {:medium => "300x300", :thumb => "100x100"},
+          :url => "/system/:class/image1/:id/:basename.:extension",
+          :path => ":rails_root/public/system/:class/image1/:id/:basename.:extension"
+
+
+
         def taxa_all
            "#{taxa_group} - #{common_name} - (#{scientific_name}) "   
         end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131007224514) do
+ActiveRecord::Schema.define(:version => 20131008212016) do
 
   create_table "counties", :force => true do |t|
     t.string   "name"
@@ -137,6 +137,9 @@ ActiveRecord::Schema.define(:version => 20131007224514) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_file_size"
+    t.string   "image_content_type"
   end
 
   create_table "users", :force => true do |t|
