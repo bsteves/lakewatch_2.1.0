e8133ccf30891b25fd93e7128da7b6c3ae427844
bsteves <bsteves@gmail.com>
2013-12-17 15:04:53 -0800

    fixing dynamic javascript routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sites do |site|
     site.resources :samplings
   end
-
   
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -62,7 +61,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-   
+     
   map.connect ':controller/:action'
   map.connect ':controller/:action/:id'
+  map.connect ':controller/:action.:format'
 end
