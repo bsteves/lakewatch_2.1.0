ec54ffcb8b452cff1a12bbe0cb532ef0fcdcd9af
bsteves <bsteves@gmail.com>
2013-12-17 17:16:23 -0800

    fix

--- app/controllers/mydata_controller.rb
+++ app/controllers/mydata_controller.rb
@@ -5,8 +5,8 @@ before_filter :login_required, :except => [:index, :show]
 
 
   def index
-    @mysamplings = Sampling.all(:conditions => ['user_id = ?', current_user])
-    @mysites = Site.all(:conditions => ['user_id = ?', current_user])
+    @mysamplings = Sampling.all(:conditions => ['user_id = ?', current_user], :order => ['date DESC'])
+    @mysites = Site.find(:all, :conditions => ['user_id = ?', current_user], :order => ['lake_id, name'])
 
   end
 
