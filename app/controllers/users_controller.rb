d4e6db43ccb587d5f0473194d5bb0931129fe829
Brian Steves <bsteves@circe.rc.pdx.edu>
2013-12-19 11:08:39 -0800

    adding Vanessa to taxon edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  def index
+    @users = User.all
+  end
   
   def create
     @user = User.new(params[:user])
