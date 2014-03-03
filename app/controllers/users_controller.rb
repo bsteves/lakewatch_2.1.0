c65693ab5b028abc9a27459c231364f1d62cfe02
bsteves <bsteves@gmail.com>
2014-03-03 13:10:26 -0800

    adding rss user feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-layout "application"
+#layout "application"
 before_filter :login_required, :except => [:new, :create]
 
 
@@ -7,9 +7,9 @@ before_filter :login_required, :except => [:new, :create]
     @user = User.new
   end
   def index
-    @users = User.all
+    @users = User.find(:all, :order => "created_at DESC") 
   end
-
+  
   def show
     @user = User.find(params[:id])
   end
