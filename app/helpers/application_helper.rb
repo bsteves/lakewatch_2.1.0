626e9cab5fb7363e4ceefad55d2da4b960d464bd
bsteves <bsteves@gmail.com>
2014-03-13 16:36:41 -0700

    removing visualization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-include GoogleVisualization
 
 def title(title)
   content_for(:title) { title }
