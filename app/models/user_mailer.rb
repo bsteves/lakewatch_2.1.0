8783328fe04c1b60a7c195bb48c5f8d63588ff3e
bsteves <bsteves@gmail.com>
2014-06-02 15:50:50 -0700

    updated ordering of taxa in observations form

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -7,4 +7,12 @@ def send_email(user)
     :body => render_message("registration_confirmation", :user => user)
 end  
 
+def send_password_reset(user)
+    recipients user.email
+    from  "bsteves@pdx.edu"
+    subject "Lake Watch Password Reset Link"
+    part :content_type => "text/html",
+      :body => render(message("password_reset", :user => user)
+end
+
 end
